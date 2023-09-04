b4b14a4fa6787a669d260e528d0920e30a983d8d
Peter Krautzberger <p.krautzberger@gmail.com>
2023-09-04 16:26:11 +0200

    chore(img.ts): prettify

--- ts/img.ts
+++ ts/img.ts
@@ -22,13 +22,11 @@
  * @author peter@krautzource.com (Peter Krautzberger)
  */
 
-
 import { Configuration } from "mathjax-full/mjs/input/tex/Configuration.js";
 import { CommandMap } from "mathjax-full/mjs/input/tex/SymbolMap.js";
 import TexError from "mathjax-full/mjs/input/tex/TexError.js";
 import TexParser from "mathjax-full/mjs/input/tex/TexParser.js";
 import { ParseMethod } from "mathjax-full/mjs/input/tex/Types.js";
-
 
 let imgMethods: Record<string, ParseMethod> = {};
 
@@ -53,45 +51,41 @@ interface def {
 }
 
 imgMethods.Img = function (parser: TexParser, name: string) {
-    const arg = parser.GetBrackets(name, "");
-    const def: def = {
-      src: "",
-      alt: "",
-      valign: "",
-      width: "",
-      height: "",
-    };
-    if (arg.match(/[,=]/)) {
-      for (const pair of arg.split(/\s*,\s*/)) {
-        const [key, ...value] = pair.split(/\s*=\s*/);
-        if (!def.hasOwnProperty(key))
-          throw new TexError(
-            "UnknownKey",
-            'Unknown parameter "%1" in %2',
-            key,
-            name
-          );
-        def[key] = CheckDimen(value.join("="));
-      }
-    } else {
-      def.valign = CheckDimen(arg);
-      def.width = CheckDimen(parser.GetBrackets(name, ""));
-      def.height = CheckDimen(parser.GetBrackets(name, ""));
-    }
-    def.alt = parser.GetBrackets(name, "");
-    def.src = parser.GetArgument(name);
-    if (!def.valign) delete def.valign;
-    if (!def.width) delete def.width;
-    if (!def.height) delete def.height;
-    if (!def.alt) delete def.alt;
-    parser.Push(parser.create("token", "mglyph", def));
+  const arg = parser.GetBrackets(name, "");
+  const def: def = {
+    src: "",
+    alt: "",
+    valign: "",
+    width: "",
+    height: "",
   };
+  if (arg.match(/[,=]/)) {
+    for (const pair of arg.split(/\s*,\s*/)) {
+      const [key, ...value] = pair.split(/\s*=\s*/);
+      if (!def.hasOwnProperty(key))
+        throw new TexError(
+          "UnknownKey",
+          'Unknown parameter "%1" in %2',
+          key,
+          name
+        );
+      def[key] = CheckDimen(value.join("="));
+    }
+  } else {
+    def.valign = CheckDimen(arg);
+    def.width = CheckDimen(parser.GetBrackets(name, ""));
+    def.height = CheckDimen(parser.GetBrackets(name, ""));
+  }
+  def.alt = parser.GetBrackets(name, "");
+  def.src = parser.GetArgument(name);
+  if (!def.valign) delete def.valign;
+  if (!def.width) delete def.width;
+  if (!def.height) delete def.height;
+  if (!def.alt) delete def.alt;
+  parser.Push(parser.create("token", "mglyph", def));
+};
 
-new CommandMap(
-  "img",
-  { img: "Img" },
-  imgMethods
-);
+new CommandMap("img", { img: "Img" }, imgMethods);
 
 export const configuration = Configuration.create("img", {
   handler: { macro: ["img"] },
